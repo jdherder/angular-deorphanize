f42338f3eafbac2c996f38991b9fac2a0cba391d
Josh Herder <jdherder@gmail.com>
2016-12-06 00:06:41 -0600

    Init commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# angular-deorphanize
+
+An Angular 1.x filter (contained in a standalone module) to prevent orphaned text. An optional parameter can be used to increase the extent of orphan prevention, the number of additional words to keep at the end of the string from breaking.
+
+Add the module to your app as a dependency:
+
+    angular
+        .module('app', [
+            'deorphanizeFilter',
+            'app.otherModule'
+        ]);
+        
+Then, in a partial, utilize the filter to prevent an orphaned word in a string:
+
+    <p class="text-large" ng-bind-html="vm.test | deorphanize"></p>
+    
+    
+Additionally, the 'strength' can be increased with an optional parameter:
+
+    <p>{{ vm.test | deorphanize:3 }}</p>
--- deorphanize.js
+++ deorphanize.js
@@ -1,7 +1,7 @@
 (function() {
     angular
-        .module('deophanizeFilter', [])
-        .filter('deophanize', function () {
+        .module('deorphanizeFilter', [])
+        .filter('deorphanize', function () {
             return function (input, n) {
 
                 if (!input) {
@@ -9,25 +9,25 @@
                 }
 
                 var loops = parseInt(n) || 1;
-                var deophanize = input;
+                var deorphanize = input;
                 var tags = input.match(/<(.|\n)*?>/g);
                 var placeholders;
 
                 for (var h = 0; h < tags.length; h++) {
-                    deophanize = deophanize.replace(tags[h], '###' + h + '###');
+                    deorphanize = deorphanize.replace(tags[h], '###' + h + '###');
                 }
 
                 for (var i = 0; i < loops; i++) {
-                    deophanize = deophanize.replace(/\s(?=[^\s]*$)/, '&nbsp;');
+                    deorphanize = deorphanize.replace(/\s(?=[^\s]*$)/, '&nbsp;');
                 }
 
-                placeholders = deophanize.match(/###\d###/g);
+                placeholders = deorphanize.match(/###\d###/g);
 
                 for (var j = 0; j < placeholders.length; j++) {
-                    deophanize = deophanize.replace(placeholders[j], tags[j]);
+                    deorphanize = deorphanize.replace(placeholders[j], tags[j]);
                 }
 
-                return deophanize;
+                return deorphanize;
             }
         });
 })();
